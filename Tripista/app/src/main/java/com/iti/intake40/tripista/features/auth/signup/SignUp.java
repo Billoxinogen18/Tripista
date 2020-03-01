8e2e7a015f4bf61009f0540923902da0c2cb6e76
mahmoud <Mahmoudmsayed0@gmail.com>
2020-03-01 20:03:12 +0200

    add sign up activity

---
+++ Tripista/app/src/main/java/com/iti/intake40/tripista/features/auth/signup/SignUp.java
@@ -0,0 +1,16 @@
+package com.iti.intake40.tripista.features.auth.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iti.intake40.tripista.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
