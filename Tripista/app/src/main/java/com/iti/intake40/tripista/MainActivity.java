74ba6942d96e05237632ed4b94f08bfa1942105e
RemonShehata <RemonGerges94@gmail.com>
2020-03-01 13:40:29 +0200

    first commit

---
+++ Tripista/app/src/main/java/com/iti/intake40/tripista/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iti.intake40.tripista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
