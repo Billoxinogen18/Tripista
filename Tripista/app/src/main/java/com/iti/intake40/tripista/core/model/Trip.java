2ca1f51280d2dc377f51ef44227a9fa705f2f893
RemonShehata <RemonGerges94@gmail.com>
2020-03-16 14:42:58 +0200

    Refector Trip model

--- Tripista/app/src/main/java/com/iti/intake40/tripista/core/model/Trip.java
+++ Tripista/app/src/main/java/com/iti/intake40/tripista/core/model/Trip.java
@@ -4,27 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Trip {
-    String tripId;
-    String title;
-    String date;
-    String time;
-    String startPoint;
-    String endPoint;
-    String backStartPoint;
-    String backEndPoint;
-    String status; //upcomming / done / cancelled
-    String type; //one way / round trip
-    String latLng;
-    String backTime;
-    String backDate;
 
+    /*
+    private fields
+     */
+    private String tripId;
+    private String title;
+    private String date;
+    private String time;
+    private String startPoint;
+    private String endPoint;
+    private String backStartPoint;
+    private String backEndPoint;
+    private int status; //upcomming / done / cancelled
+    private int type; //one way / round trip
+    private String latLng;
+    private String backTime;
+    private String backDate;
+    private List<Note> notes = new ArrayList<>();
 
-    List<Note> notes = new ArrayList<>();
+    /*
+    enums
+     */
+    public enum Type {
+        ONE_WAY,
+        ROUND_TRIP
+    }
+
+    public enum Status {
+        UPCOMMING,
+        DONE,
+        CANCELLED
+    }
+
+    /*
+    constructors
+     */
 
     public Trip() {
     }
 
-    public Trip(String tripId, String title, String date, String time, String startPoint, String endPoint, String backStartPoint, String backEndPoint, String status, String type, String latLng, String backTime, String backDate, List<Note> notes) {
+    public Trip(String tripId, String title, String date, String time, String startPoint, String endPoint, String backStartPoint, String backEndPoint, int status, int type, String latLng, String backTime, String backDate, List<Note> notes) {
         this.tripId = tripId;
         this.title = title;
         this.date = date;
@@ -41,34 +61,9 @@ public class Trip {
         this.notes = notes;
     }
 
-    public Trip(String date, String time) {
-        this.date = date;
-        this.time = time;
-    }
-
-    public String getLatLng() {
-        return latLng;
-    }
-
-    public void setLatLng(String latLng) {
-        this.latLng = latLng;
-    }
-
-    public String getBackStartPoint() {
-        return backStartPoint;
-    }
-
-    public void setBackStartPoint(String backStartPoint) {
-        this.backStartPoint = backStartPoint;
-    }
-
-    public String getBackEndPoint() {
-        return backEndPoint;
-    }
-
-    public void setBackEndPoint(String backEndPoint) {
-        this.backEndPoint = backEndPoint;
-    }
+    /*
+    getters and setters
+     */
 
     public String getTripId() {
         return tripId;
@@ -114,21 +109,48 @@ public class Trip {
         return endPoint;
     }
 
-
     public void setEndPoint(String endPoint) {
         this.endPoint = endPoint;
     }
 
-    public String getStatus() {
+    public String getBackStartPoint() {
+        return backStartPoint;
+    }
+
+    public void setBackStartPoint(String backStartPoint) {
+        this.backStartPoint = backStartPoint;
+    }
+
+    public String getBackEndPoint() {
+        return backEndPoint;
+    }
+
+    public void setBackEndPoint(String backEndPoint) {
+        this.backEndPoint = backEndPoint;
+    }
+
+    public int getStatus() {
         return status;
     }
 
-    public String getType() {
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getType() {
         return type;
     }
 
-    public List<Note> getNotes() {
-        return notes;
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(String latLng) {
+        this.latLng = latLng;
     }
 
     public String getBackTime() {
@@ -145,5 +167,13 @@ public class Trip {
 
     public void setBackDate(String backDate) {
         this.backDate = backDate;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
     }
 }
