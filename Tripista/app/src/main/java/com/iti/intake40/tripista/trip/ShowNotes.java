8efb8f1c803776328577c7b9453b0a94b3f0089a
mahmoud <Mahmoudmsayed0@gmail.com>
2020-03-18 21:31:48 +0200

    show notes

---
+++ Tripista/app/src/main/java/com/iti/intake40/tripista/trip/ShowNotes.java
@@ -0,0 +1,16 @@
+package com.iti.intake40.tripista.trip;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.iti.intake40.tripista.R;
+
+public class ShowNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_notes);
+    }
+}
