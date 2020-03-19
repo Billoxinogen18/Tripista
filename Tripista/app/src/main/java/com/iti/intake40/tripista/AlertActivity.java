e01c7ad4cb250ef9e0e2a3277fad5af969e45cc5
RemonShehata <RemonGerges94@gmail.com>
2020-03-19 22:06:04 +0200

    slight refactoring in AlertActivity

--- Tripista/app/src/main/java/com/iti/intake40/tripista/AlertActivity.java
+++ Tripista/app/src/main/java/com/iti/intake40/tripista/AlertActivity.java
@@ -15,9 +15,13 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.WindowManager;
+
 import androidx.appcompat.app.AlertDialog;
+
 import com.iti.intake40.tripista.core.FireBaseCore;
+import com.iti.intake40.tripista.core.model.Trip;
 import com.iti.intake40.tripista.map.ShowMap;
+
 import java.util.Random;
 import java.util.UUID;
 
@@ -75,30 +79,30 @@ public class AlertActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(intentExtra).setCancelable(
-                false).setPositiveButton("Start", new DialogInterface.OnClickListener() {
+                false).setPositiveButton(R.string.start, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 if (getIntent() != null) {
                     String tripId = getIntent().getExtras().getString("id");
-                    core.changeStateOfTrip("DONE", tripId);
+                    core.changeStateOfTrip(Trip.Status.DONE.toString(), tripId);
                     Intent goMap = new Intent(AlertActivity.this, ShowMap.class);
                     goMap.putExtra("id", tripId);
                     startActivity(goMap);
                     finish();
                 }
             }
-        }).setNeutralButton("Snooze",
+        }).setNeutralButton(R.string.snooze,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         String tripId = getIntent().getExtras().getString("id");
-                        core.changeStateOfTrip("INPROGRESS", tripId);
+                        //core.changeStateOfTrip(, tripId);
                         id = notificationId;
                         show_Notification();
                         ringtone.stop();
                     }
 
 
-                }).setNegativeButton("Stop",
+                }).setNegativeButton(R.string.stop,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         String tripId = getIntent().getExtras().getString("id");
@@ -150,7 +154,7 @@ public class AlertActivity extends Activity {
         } else {
             PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             Notification notification = new Notification.Builder(getApplicationContext())
-                    .setContentText("You are waiting for your trip")
+                    .setContentText("Your trip is waiting")
                     .setContentTitle(intentExtra)
                     .setContentIntent(pendingIntent)
                     .setVibrate(new long[]{1000, 1000})
@@ -170,7 +174,6 @@ public class AlertActivity extends Activity {
     protected void onStop() {
         super.onStop();
         ringtone.stop();
-//        finish();
     }
 
     public static String generateString() {
